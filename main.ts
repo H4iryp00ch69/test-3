f860a3827ad30d7dcc7d95957c4ae5a0216edad9
H4iryp00ch69 <67195769+H4iryp00ch69@users.noreply.github.com>
2020-06-23 08:58:14 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    if (0 < 0) {
+        
+    } else {
+        
+    }
+    
+})
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    
+    time = "" + ("" + hours) + (":" + ("" + minutes))
+    basic.showString(time)
+})
+let hours = 0
+let minutes = 0
+let time = ""
+let ampm = false
+time = ""
+let adjust = 0
+minutes = 0
+hours = 0
